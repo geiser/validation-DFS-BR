6e71ec75972b1e79dfb0d7562e20326ddc87118c
Challco C. G <geiser@alumni.usp.br>
2021-04-19 15:13:53 -0300

    update scripts

--- script.R
+++ script.R
@@ -1,16 +1,13 @@
+## ----setup, include=FALSE, echo=FALSE-----------------------------------------------
 wants <- c('psych', 'dplyr','readxl','psy','olsrr','MVN','parameters')
 has <- wants %in% rownames(installed.packages())
 if (any(!has)) install.packages(wants[!has])
 options("mc.cores"=parallel::detectCores())
 
-knitr::opts_chunk$set(echo = TRUE)
-defaultW <- getOption("warn")
-options(warn = -1)
 
-# Prerequisites
 
-## Loading libs
 
+## ----message=FALSE------------------------------------------------------------------
 library(readr)
 library(dplyr)
 library(psych)
@@ -19,36 +16,33 @@ library(olsrr)
 library(semTools)
 library(mirt)
 
-## Loading data
 
+## -----------------------------------------------------------------------------------
 raw_data <- read.csv("data-dfs.csv")
 dfs <- as.data.frame(sapply(select(raw_data, starts_with("Q")), as.integer))
 
-## Checking assumptions 
 
-### Evaluating normality
-
+## -----------------------------------------------------------------------------------
 (mvn_mod <- MVN::mvn(dfs))
 
-### Performing Kaiser, Meyer, Olkin (KMO) Measure of Sampling Adequacy 
 
+## -----------------------------------------------------------------------------------
 (kmo_mod <- KMO(dfs)) 
 
-### Summarizing descriptives and KMOs
 
-t(round(cbind(mvn_mod$Descriptives[,c("Mean","Std.Dev","Median","Skew","Kurtosis")],"MSA"=kmo_mod$MSAi),2))
+## ---- echo=FALSE--------------------------------------------------------------------
+(df <- cbind(mvn_mod$Descriptives[,c("Mean","Std.Dev","Median","Skew","Kurtosis")]
+             , mvn_mod$univariateNormality[,c("Statistic","p value")]
+             , "MSAi"=kmo_mod$MSAi))
 
 
-### Performing Bartlett's test of sphericity
 
-(parameters::check_sphericity(dfs)) # *bartlett.test()* performs test of homogeneity of variances
 
-# Construct validity
+## -----------------------------------------------------------------------------------
+(parameters::check_sphericity(dfs)) 
 
-## Structural validity
 
-### CFA in the multicorrelated model
-
+## -----------------------------------------------------------------------------------
 Modelo1 <- '
 CSB =~ Q1 + Q10 + Q19 + Q28
 MAA =~ Q2 + Q11 + Q20 + Q29
@@ -108,6 +102,8 @@ TT ~~ AE
 fit1 <-cfa(Modelo1, data=dfs,estimator="WLSMV", std.lv=TRUE)
 standardizedSolution(fit1)
 
+
+## -----------------------------------------------------------------------------------
 Modelo1a <- '
 CSB =~ Q1 + Q10 + Q19 + Q28
 MAA =~ Q2 + Q11 + Q20 + Q29
@@ -167,8 +163,8 @@ TT ~~ 1*AE
 fit1a <-cfa(Modelo1a, data=dfs,estimator="WLSMV", std.lv=TRUE)
 standardizedSolution(fit1a)
 
-### CFA in the 2nd-order model
 
+## -----------------------------------------------------------------------------------
 Modelo2 <- '
 CSB =~ Q1 + Q10 + Q19 + Q28
 MAA =~ Q2 + Q11 + Q20 + Q29
@@ -229,46 +225,50 @@ TT ~~ 0*AE
 fit2 <-cfa(Modelo2, data=dfs,estimator="WLSMV", std.lv=TRUE)
 standardizedSolution(fit2)
 
-### CFA in the short version of DFS-BR (DFS-short)
 
+## -----------------------------------------------------------------------------------
 Modelo3 <- 'DFS =~ Q19 + Q29 + Q12 + Q22 + Q32 + Q6 + Q7 + Q17 + Q36'
 
 fit3 <-cfa(Modelo3, data=dfs,estimator="WLSMV", std.lv=TRUE)
 standardizedSolution(fit3)
 
-### CFA in an alternative short version of DFS-BR (DFS-short2)
 
+## -----------------------------------------------------------------------------------
 Modelo4 <- 'DFS =~ Q10 + Q20 + Q21 + Q4 + Q32 + Q24 + Q7 + Q17 + Q36'
 
 fit4 <-cfa(Modelo4, data=dfs,estimator="WLSMV", std.lv=TRUE)
 standardizedSolution(fit4)
 
-sl <- standardizedSolution(fit4)
-sl <- sl$est.std[sl$op == "=~"]
-(describe(sl))
 
-### Summarize the fit indexes and draw the diagrams of CFA models
-
+## ---- echo=FALSE--------------------------------------------------------------------
 df_fit <- do.call(rbind, lapply(c(fit1a,fit2,fit3,fit4), FUN = function(fit) {
-  dat <- as.list(round(fitMeasures(fit, c("chisq","df","rni","cfi","tli","srmr"
+  dat <- as.list(round(fitMeasures(fit, c("chisq","df","gfi","agfi","rni","cfi","tli","srmr"
                                           ,"rmsea","rmsea.ci.lower","rmsea.ci.upper")), 3))
   rbind(c(dat[c("chisq","df")],"chisq/df"=round(dat$chisq/dat$df,3)
-          , dat[c("rni","cfi","tli","srmr","rmsea")]
+          , dat[c("gfi","agfi","cfi","rni","tli","srmr","rmsea")]
           , "rmsea.ci" = paste0("[",dat$rmsea.ci.lower,"; ",dat$rmsea.ci.upper,"]")))
 }))
-rownames(df_fit) <- c("multicorr","2nd-order","unidim1", "unidim2")
+rownames(df_fit) <- c("multicorrelated 9-factors","2nd-order 9-factors"
+                      ,"original short ver", "alternative short ver")
 (df_fit)
 
+
+
+
+## -----------------------------------------------------------------------------------
 anova(fit1a,fit2)
 
 
-## Internal consistency (reliability)
-
+## -----------------------------------------------------------------------------------
 reliability(fit1a, return.total = T)
 reliabilityL2(fit2, 'F1')
 
+
+## -----------------------------------------------------------------------------------
 reliability(fit4, return.total = T)
 
+
+## -----------------------------------------------------------------------------------
 Fatores <- list(
   'CSB'   = c("Q1","Q10","Q19","Q28")
   , 'MAA' = c("Q2","Q11","Q20","Q29")
@@ -300,15 +300,18 @@ compReliability <- function(fit, lfactors = c(), return.total = F) {
 }
 
 (compReliability(fit1a, Fatores, return.total = T))
+
 (compReliability(fit4, c(), return.total = T))
 
 
-df.rel <- rbind(reliability(fit1a, return.total = T),
-                "CR"=compReliability(fit1a, Fatores, return.total = T))
-(round(df.rel, 3))
+## ---- echo=FALSE--------------------------------------------------------------------
+(df.rel <- rbind(reliability(fit1a, return.total = T)
+                 , "CR"=compReliability(fit1a, Fatores, return.total = T)))
 
-## Convergent validity and discriminant validity
 
+
+
+## -----------------------------------------------------------------------------------
 convergentDiscriminantValidity <- function(fit, lvn, factors, dat) {
   library(olsrr)
   library(semTools)
@@ -341,32 +344,45 @@ convergentDiscriminantValidity <- function(fit, lvn, factors, dat) {
 
 convergentDiscriminantValidity(fit1a, Modelo1a, Fatores, dat = dfs)
 
-# Item quality analysis based on IRT
 
-## IRT analysis in the long version of the DFS-BR
+## ---- echo=FALSE--------------------------------------------------------------------
+(df <- convergentDiscriminantValidity(fit1a, Modelo1a, Fatores, dat = dfs))
 
+
+
+
+## -----------------------------------------------------------------------------------
 dados_tri<-mirt(dfs, 1, itemtype='graded')
 
-### Estimate parameters
 
+## ---- echo=FALSE--------------------------------------------------------------------
 (params <- coef(dados_tri, simplify=TRUE, IRTpars=TRUE))
 
-### Graphs of test information curve
 
+
+
+## ---- fig.width=12------------------------------------------------------------------
 plot(dados_tri, type='infotrace')
-plot(dados_tri, type='info')
+
+
+## ---- fig.width=12------------------------------------------------------------------
 plot(dados_tri, type='infoSE')
 
-## IRT analysis in the short version of the DFS-BR
 
+## -----------------------------------------------------------------------------------
 dados_tri2 <-mirt(dfs[,c("Q10","Q20","Q21","Q4","Q32","Q24","Q7","Q17","Q36")], 1, itemtype='graded')
 
-### Estimate parameters
 
+## ---- echo=FALSE--------------------------------------------------------------------
 (params2 <- coef(dados_tri2, simplify=TRUE, IRTpars=TRUE))
 
-### Graphs of test information curve
 
+
+
+## ---- fig.width=12------------------------------------------------------------------
 plot(dados_tri2,type='infotrace')
-plot(dados_tri2,type='info')
+
+
+## ---- fig.width=12------------------------------------------------------------------
 plot(dados_tri2,type='infoSE')
+
